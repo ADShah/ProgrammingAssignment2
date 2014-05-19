56391317e7b79cc5b5261ea83844048e15f1b185
ADShah <path4peace@gmail.com>
2014-05-19 14:42:49 -0400

    first draft

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The pair of functions, makeCacheMatrix and cachesolve, can be used to cache
+## inverse of a matrix, so it does not have to be computed repeatedly
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special "matrix" that that can be used to cache the
+## inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    im <- NULL
+    set <- function(y) {
+        x <<- y
+        im <<- NULL
+    }
+    get <- function() x
+    setinvmatrix <- function(invmatrix) im <<- invmatrix
+    getinvmatrix <- function() im
+    list(set = set, get = get, 
+         setinvmatrix = setinvmatrix,
+         getinvmatrix = getinvmatrix)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve will compute the inverse of a "matrix" returned by makeCacheMatrix.
+## If the inverse has already been calculated and the matrix has not changed,
+## the inverse is retrieved from the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    im <- x$getinvmatrix()
+    if(!is.null(im)) {
+        message("getting cached data")
+        return(im)
+    }
+    else {
+        data <- x$get()
+        ## rows and columns must be the same to create an inverse
+        r <- nrow(data)
+        c <- ncol(data) 
+        if (r == c) {
+            ## need to create an identity matrix
+            identm <- matrix(0, r, c)
+            identm <- diag(r)
+            im <- solve(data, identm, ...)
+            x$setinvmatrix(im)
+        }
+        im
+    }
 }
